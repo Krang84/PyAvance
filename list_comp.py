197396332d81d8aa80ee521fc7bcd2aa25b188aa
Krang84 <patelassad@gmail.com>
2023-01-05 13:33:40 +0100

    Update list_comp.py

--- list_comp.py
+++ list_comp.py
@@ -2,3 +2,11 @@
 
 # list is a container sequence 
 # list mutable 
+# how to
+my_sequence="python"
+my_list=[]
+for i in my_sequence:
+  my_list.append(i)
+  
+# same list
+[i for i in my_sequence]
