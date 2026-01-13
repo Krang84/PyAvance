0a9a983256ab722ab333c46359720e9188a443f3
Krang84 <patelassad@gmail.com>
2026-01-13 15:11:51 +0100

    Update list_comp.py

--- list_comp.py
+++ list_comp.py
@@ -1,12 +1,5 @@
 # How to List comp
-
-# list is a container sequence 
-# list mutable 
-# how to
-my_sequence="python"
-my_list=[]
-for i in my_sequence:
-  my_list.append(i)
-  
-# same list
-[i for i in my_sequence]
+sequence = 'a*ù^$^'
+nb = [ord(i) for i in sequence]
+print(nb)
+# [97, 42, 249, 94, 36, 94]
