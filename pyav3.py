d944e7ee68da1f08054e380021b62cb938ece0ed
Krang84 <patelassad@gmail.com>
2026-02-09 11:20:03 +0100

    Create pyav3.py

---
+++ pyav3.py
@@ -0,0 +1,11 @@
+symbols='^$*ù!:;,'
+for i in (f'Valeur en utf-8 : {ord(i)}' for i in symbols):
+    print(i)
+#Valeur en utf-8 : 94
+#Valeur en utf-8 : 36
+#Valeur en utf-8 : 42
+#Valeur en utf-8 : 249
+#Valeur en utf-8 : 33
+#Valeur en utf-8 : 58
+#Valeur en utf-8 : 59
+#Valeur en utf-8 : 44
