be881c69d22bc2181f2d44c8615ab6d554f8157b
Krang84 <patelassad@gmail.com>
2026-02-26 14:58:20 +0100

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,26 +1,26 @@
 # (country_code, passeport_number)
 
-traveler = [('USA','316665644'),('BRA','454654654154'),('ESP','45454654XS')]
+#traveler = [('USA','316665644'),('BRA','454654654154'),('ESP','45454654XS')]
 
-
+# record tuples
 city, year, pop, chg, area=('Tokyo', 2003, 32450, 0.66,8014)
 
 lax_coordinates=(33.9425, -118.408056)
-latitude, longitude=lax_coordinates
+#latitude, longitude=lax_coordinates
 
-metro_areas=[('Tokyo','JP',36.9333,(35.565656,139.46565565)),
+#metro_areas=[('Tokyo','JP',36.9333,(35.565656,139.46565565)),
              ('Delhi NCR','IN',21.935,(28.226656,77.22266652)),
              ('Mexico City','MX',20.142,(19.43333,-99.2223665)),
              ('New York-Newark','US',20.104,(40.808611,-74.0233665)),
              ('Sao Paulo','BR',19.649,(-23.2222366,-46.233366)),]
 
-for i,j,k,l in metro_areas:
-  print(i,j,k,l)
+#for i,j,k,l in metro_areas:
+ # print(i,j,k,l)
 
-for i,j,k,(lat, lon) in metro_areas:
-    if lon <=0:
-        print(i, " : ",lat, lon)
+#for i,j,k,(lat, lon) in metro_areas:
+  #  if lon <=0:
+   #     print(i, " : ",lat, lon)
 
-		
-[print(a,b) for _,_,_,(a,b) in metro_areas]
+		#
+#[print(a,b) for _,_,_,(a,b) in metro_areas]
 
